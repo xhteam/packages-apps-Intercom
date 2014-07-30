9fba046cff78fbbc8880916710fa3f0886e377bd
Gandaquan <gandaquan@xhinfo.com>
2014-07-30 18:42:57 +0800

    remove OnKeyEvenHappened.java

--- src/com/android/intercom/callback/OnKeyEvenHappened.java
+++
@@ -1,8 +0,0 @@
-package com.android.intercom.callback;
-
-import android.view.KeyEvent;
-
-public interface OnKeyEvenHappened {
-	public void onKeyDown(int keyCode, KeyEvent keyEvent);
-	public void onKeyUp(int keyCode, KeyEvent keyEvent);
-}
